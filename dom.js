a22133ba5c244e7d3c793f4953c7e8ab7d83de80
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-11 22:06:30 +0100

    updated

--- dom.js
+++ dom.js
@@ -5,3 +5,11 @@ console.log(body.innerHTML);
 // body.innerHTML = `<h1 class="heading">Hello my name is DOM</h1>`
 const container= document.querySelector(".container")
 console.log({container});
+console.log(container.childElementCount);
+console.log(container.nextElementSibling);
+console.log(container.parentElement);
+
+container.addEventListener("click",function(event){
+console.log(event);
+console.log("you clicked me just now");
+})
