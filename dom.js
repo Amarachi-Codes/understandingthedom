84cc9eb30eae437bca204ef45bdc6b358261c08c
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-11 19:41:43 +0100

    understanding the dom

---
+++ dom.js
@@ -0,0 +1,7 @@
+// console.log({document,window});
+const body = document.querySelector('body')
+// console.log({body});
+console.log(body.innerHTML);
+// body.innerHTML = `<h1 class="heading">Hello my name is DOM</h1>`
+const container= document.querySelector(".container")
+console.log({container});
